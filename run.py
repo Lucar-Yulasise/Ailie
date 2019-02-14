490e7709ea66f9adf43e2f7cfb00417239e44090
Mick Brujah <brujah666@outlook.com>
2019-02-14 17:00:24 +0800

    更改了一些代码结构

--- run.py
+++ run.py
@@ -2,10 +2,24 @@ import requests
 import json
 key = "378e38a2bf6e4d2f8bff88e4ad6170f7"
 print("Ailie 正在运行")
-while True:
-	info = input("\n我：")
-	url = 'http://www.tuling123.com/openapi/api?key='+key+'&info='+info
+
+
+def get_information(url):
 	res = requests.get(url)
 	res.enconding = 'utf-8'
 	jd = json.loads(res.text)
-	print("\nAilie: "+jd['text'])
+	return jd['text']
+
+
+def main():
+	while True:
+		info = input("我：")
+		url = 'http://www.tuling123.com/openapi/api?key='+key+'&info='+info
+		word = get_information(url)
+		print("Ailie: "+word)
+		
+
+if __name__=='__main__':
+	main()
+
+	
