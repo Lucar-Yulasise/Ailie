685fefa868a012b50922d10ac3ecad3e09e394ce
Lucar <brujah666@outlook.con>
2022-01-07 22:20:47 +0800

    增加了一个语音引擎

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import requests
 import json
+import pyttsx3
+
+
 key = "378e38a2bf6e4d2f8bff88e4ad6170f7"
 print("Ailie 正在运行")
 
@@ -12,12 +15,22 @@ def get_information(url):
 
 
 def main():
+
+	engine = pyttsx3.Engine()
+	rate = engine.getProperty('rate')
+	engine.setProperty('rate',200)
+
 	while True:
 		info = input("我：")
 		url = 'http://www.tuling123.com/openapi/api?key='+key+'&info='+info
 		word = get_information(url)
+
+
 		print("艾莉: "+word)
+		engine.say(word)
+		engine.runAndWait()
 		if info == "exit":
+			engine.stop()
 			exit(1)
 		
 
